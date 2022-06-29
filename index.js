e24c4b1993e9f856262e3356f78239c646523cc7
emonShimoul <emonshimoul@gmail.com>
2022-06-30 01:23:48 +0600

    connect to database

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json())
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.pabg0.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try{
+        await client.connect();
+        console.log('db connected successfully!!');
+    }
+    finally{
+        await client.close();
+    }
+}
+run().catch(console.dir)
 
 app.get('/', (req, res) => {
     res.send('Ema john server is running...');
